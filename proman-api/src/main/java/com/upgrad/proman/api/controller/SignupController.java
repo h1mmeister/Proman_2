0f5da7382614243c789cc5078e82b8ad534511c6
h1m <himanshu.yadav643@gmail.com>
2021-07-10 20:36:48 +0530

    working on signup controller

---
+++ proman-api/src/main/java/com/upgrad/proman/api/controller/SignupController.java
@@ -0,0 +1,22 @@
+package com.upgrad.proman.api.controller;
+
+import com.upgrad.proman.api.model.SignupUserRequest;
+import com.upgrad.proman.api.model.SignupUserResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/")
+public class SignupController {
+
+    @RequestMapping(method = RequestMethod.POST, path = "/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
