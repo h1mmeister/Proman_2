7ea813b701fb75796e93a50b75728dba6a8d609d
h1m <himanshu.yadav643@gmail.com>
2021-07-11 22:22:06 +0530

    working on the DAO class

---
+++ proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
@@ -0,0 +1,12 @@
+package com.upgrad.proman.service.business;
+
+import com.upgrad.proman.service.entity.UserEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignupBusinessService {
+
+    public UserEntity signup(UserEntity userEntity) {
+
+    }
+}
