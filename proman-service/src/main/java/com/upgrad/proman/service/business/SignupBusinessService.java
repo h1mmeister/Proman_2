878d6951b3df3f9366fda09a1bd359ef1d7a1d5f
h1m <himanshu.yadav643@gmail.com>
2021-07-12 23:18:09 +0530

    writing up the signup service class with user dao

--- proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
+++ proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
@@ -1,12 +1,17 @@
 package com.upgrad.proman.service.business;
 
+import com.upgrad.proman.service.dao.UserDao;
 import com.upgrad.proman.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignupBusinessService {
 
-    public UserEntity signup(UserEntity userEntity) {
+    @Autowired
+    private UserDao userDao;
 
+    public UserEntity signup(UserEntity userEntity) {
+        return userDao.createUser(userEntity);
     }
 }
