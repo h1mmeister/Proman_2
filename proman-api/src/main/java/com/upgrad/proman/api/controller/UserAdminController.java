c98c5dd35fc4c8867dcedc4dcc3c8138bbab7417
h1m <himanshu.yadav643@gmail.com>
2021-07-16 00:11:42 +0530

    handling the error in the proman api module using exception handler

--- proman-api/src/main/java/com/upgrad/proman/api/controller/UserAdminController.java
+++ proman-api/src/main/java/com/upgrad/proman/api/controller/UserAdminController.java
@@ -4,6 +4,7 @@ import com.upgrad.proman.api.model.UserDetailsResponse;
 import com.upgrad.proman.api.model.UserStatusType;
 import com.upgrad.proman.service.business.UserAdminBusinessService;
 import com.upgrad.proman.service.entity.UserEntity;
+import com.upgrad.proman.service.exception.ResourceNotFoundException;
 import com.upgrad.proman.service.type.UserStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,7 +23,7 @@ public class UserAdminController {
     private UserAdminBusinessService userAdminBusinessService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/users/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<UserDetailsResponse> getUser(@PathVariable("id") final String userUuid) {
+    public ResponseEntity<UserDetailsResponse> getUser(@PathVariable("id") final String userUuid) throws ResourceNotFoundException {
         final UserEntity userEntity = userAdminBusinessService.getUser(userUuid);
         UserDetailsResponse userDetailsResponse = new UserDetailsResponse().id(userEntity.getUuid()).firstName(userEntity.getFirstName()).lastName(userEntity.getLastName())
                 .emailAddress(userEntity.getEmail()).mobileNumber(userEntity.getMobilePhone()).status(UserStatusType.valueOf(UserStatus.getEnum(userEntity.getStatus()).name()));
---
+++ proman-api/src/main/java/com/upgrad/proman/api/exception/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.upgrad.proman.api.exception;
+
+import com.upgrad.proman.api.model.ErrorResponse;
+import com.upgrad.proman.service.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> resourceNotFoundException(ResourceNotFoundException exc, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(exc.getCode()).message(exc.getErrorMessage()), HttpStatus.NOT_FOUND);
+    }
+}
