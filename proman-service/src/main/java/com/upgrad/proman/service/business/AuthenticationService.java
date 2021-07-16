d59912788a9b3d0103064e1b2b108332f15a3531
h1m <himanshu.yadav643@gmail.com>
2021-07-17 04:29:19 +0530

    added AuthenticationService

---
+++ proman-service/src/main/java/com/upgrad/proman/service/business/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.upgrad.proman.service.business;
+
+import com.upgrad.proman.service.entity.UserEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    public UserEntity authenticate(final String username, final String password) {
+        
+    }
+}
