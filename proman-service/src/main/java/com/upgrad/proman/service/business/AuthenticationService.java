6bec9b55f608e98f064ab4e07a86e3f08883eae0
h1m <himanshu.yadav643@gmail.com>
2021-07-17 05:00:42 +0530

    adding JWT token provider

--- proman-service/src/main/java/com/upgrad/proman/service/business/AuthenticationService.java
+++ proman-service/src/main/java/com/upgrad/proman/service/business/AuthenticationService.java
@@ -1,12 +1,30 @@
 package com.upgrad.proman.service.business;
 
+import com.upgrad.proman.service.dao.UserDao;
 import com.upgrad.proman.service.entity.UserEntity;
+import com.upgrad.proman.service.exception.AuthenticationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
 
-    public UserEntity authenticate(final String username, final String password) {
+    @Autowired
+    private UserDao userDao;
 
+    @Autowired
+    private PasswordCryptographyProvider cryptographyProvider;
+
+    public UserEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
+        UserEntity userEntity = userDao.getUserByEmail(username);
+        if (userEntity == null) {
+            throw new AuthenticationFailedException("ATH-001", "USER WITH EMAIL NOT FOUND.");
+        }
+        String encryptedPassword = cryptographyProvider.encrypt(password, userEntity.getSalt());
+        if (encryptedPassword.equals(userEntity.getPassword())){
+
+        } else {
+            throw new AuthenticationFailedException("ATH-002", "WRONG PASSWORD");
+        }
     }
 }
