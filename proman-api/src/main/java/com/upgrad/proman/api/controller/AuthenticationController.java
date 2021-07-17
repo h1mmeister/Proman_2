675dd4bd76af75e84de3dee5d743a1b533c7443c
h1m <himanshu.yadav643@gmail.com>
2021-07-17 06:00:43 +0530

    completed the authentication controller

--- proman-api/src/main/java/com/upgrad/proman/api/controller/AuthenticationController.java
+++ proman-api/src/main/java/com/upgrad/proman/api/controller/AuthenticationController.java
@@ -1,18 +1,31 @@
 package com.upgrad.proman.api.controller;
 
 import com.upgrad.proman.api.model.AuthorizedUserResponse;
+import com.upgrad.proman.service.business.AuthenticationService;
+import com.upgrad.proman.service.exception.AuthenticationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/")
 public class AuthenticationController {
 
-    @RequestMapping(method = RequestMethod.POST, path = "/auth/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<AuthorizedUserResponse> login(final String authorization) {
+    @Autowired
+    private AuthenticationService authenticationService;
 
+    @RequestMapping(method = RequestMethod.POST, path = "/auth/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AuthorizedUserResponse> login(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
+        byte[] decode = Base64.getDecoder().decode(authorization.split("Basic ")[1]);
+        String decodeText = new String(decode);
+        String[] decodeArray = decodeText.split(":");
+        authenticationService.authenticate(decodeArray[0], decodeArray[1]);
+        return null;
     }
 }
