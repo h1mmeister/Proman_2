8a0408ac9511b7110038735eb168839cb038355d
h1m <himanshu.yadav643@gmail.com>
2021-07-18 04:54:11 +0530

    refactored singup business service

--- proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
+++ proman-service/src/main/java/com/upgrad/proman/service/business/SignupBusinessService.java
@@ -1,6 +1,5 @@
 package com.upgrad.proman.service.business;
 
-import com.upgrad.proman.service.dao.UserDao;
 import com.upgrad.proman.service.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,18 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class SignupBusinessService {
 
     @Autowired
-    private UserDao userDao;
-
-    @Autowired
-    private PasswordCryptographyProvider cryptographyProvider;
+    private UserAdminBusinessService userAdminBusinessService;
 
     @Transactional(propagation = Propagation.REQUIRED)
     public UserEntity signup(UserEntity userEntity) {
-
-        String[] encryptedText = cryptographyProvider.encrypt(userEntity.getPassword());
-        userEntity.setSalt(encryptedText[0]);
-        userEntity.setPassword(encryptedText[1]);
-
-        return userDao.createUser(userEntity);
+        return userAdminBusinessService.createUser(userEntity);
     }
 }
--- proman-service/src/main/java/com/upgrad/proman/service/business/UserAdminBusinessService.java
+++ proman-service/src/main/java/com/upgrad/proman/service/business/UserAdminBusinessService.java
@@ -5,12 +5,17 @@ import com.upgrad.proman.service.entity.UserEntity;
 import com.upgrad.proman.service.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserAdminBusinessService {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private PasswordCryptographyProvider cryptographyProvider;
 
     public UserEntity getUser(final String userUuid) throws ResourceNotFoundException {
 
@@ -19,5 +24,19 @@ public class UserAdminBusinessService {
             throw new ResourceNotFoundException("USR-001", "USER NOT FOUND.");
         }
         return userEntity;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserEntity createUser(final UserEntity userEntity){
+
+        String password = userEntity.getPassword();
+        if (password == null) {
+            userEntity.setPassword("himanshu");
+        }
+        String[] encryptedText = cryptographyProvider.encrypt(password);
+        userEntity.setSalt(encryptedText[0]);
+        userEntity.setPassword(encryptedText[1]);
+        return userDao.createUser(userEntity);
+
     }
 }
